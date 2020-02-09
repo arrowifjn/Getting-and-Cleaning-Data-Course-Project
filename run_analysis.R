299adf83dce38ef0384fb0863c9a6ddd4e16f087
arrowif <95392782@qq.com>
2020-02-09 18:30:13 +0800

    write file

--- run_analysis.R
+++ run_analysis.R
@@ -9,43 +9,44 @@
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-# 1. Merges the training and the test sets to create one data set.
+
 # Load activity labels + features
 library(data.table)
 activityLabels <- fread("UCI HAR Dataset/activity_labels.txt", col.names = c("classLabels", "activityName"))
-features <- fread("UCI HAR Dataset/features.txt", col.names = c("index", "featureNames"))
-featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
-measurements <- features[featuresWanted, featureNames]
-measurements <- gsub('[()]', '', measurements)
+features <- fread("UCI HAR Dataset/features.txt", col.names = c("index", "featureNames")) #get all features and index table
+View(features)
+featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames]) #get mean and standard deviation col name index vector
+View(featuresWanted)
+measurements <- features[featuresWanted, featureNames] #get col name
+measurements <- gsub('[()]', '', measurements) 
+View(measurements)
 
-# Load train datasets
+# Load 3 taining datasets
 train <- fread("UCI HAR Dataset/train/X_train.txt")[, featuresWanted, with = FALSE]
 head(train)
 data.table::setnames(train, colnames(train), measurements)
 trainActivities <- fread("UCI HAR Dataset/train/Y_train.txt", col.names = c("Activity"))
 head(trainActivities)
 trainSubjects <- fread("UCI HAR Dataset/train/subject_train.txt", col.names = c("SubjectNum"))
-train <- cbind(trainSubjects, trainActivities, train)  # merge 3 table to one df
+head(trainActivities)
+trainC <- cbind(trainSubjects, trainActivities, train)  # merge 3 table to one df
 
-# Load test datasets
-test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+# Load 3 test datasets
+test <- fread("UCI HAR Dataset/test/X_test.txt")[, featuresWanted, with = FALSE]
 data.table::setnames(test, colnames(test), measurements)
-testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
-                        , col.names = c("Activity"))
-testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")
-                      , col.names = c("SubjectNum"))
-test <- cbind(testSubjects, testActivities, test)
+testActivities <- fread( "UCI HAR Dataset/test/Y_test.txt", col.names = c("Activity"))
+testSubjects <- fread( "UCI HAR Dataset/test/subject_test.txt", col.names = c("SubjectNum"))
+testC <- cbind(testSubjects, testActivities, test) # merge 3 table to one df
 
 # merge datasets
-combined <- rbind(train, test)
+combined <- rbind(trainC, testC)
 
 # Convert classLabels to activityName basically. More explicit. 
-combined[["Activity"]] <- factor(combined[, Activity]
-                                 , levels = activityLabels[["classLabels"]]
-                                 , labels = activityLabels[["activityName"]])
-
+#combined[["Activity"]]
+combined[["Activity"]] <- factor(combined[, Activity], levels = activityLabels[["classLabels"]] , labels = activityLabels[["activityName"]])
+#combined[["Activity"]]
 combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
 combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
 combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
 
-data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE)
+data.table::fwrite(x = combined, file = "tidyData.csv", quote = FALSE)
